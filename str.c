8cf6a7ff5c024a35b33467f225209794e44caba8
mix <nomail>
2015-12-14 22:58:37 +0900

    メソッド名を修正。

--- str.c
+++ str.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include "str.h"
 
-static void _str_init(STRING *);
-static void _add_more_size(STRING *);
-static int _is_max_size(STRING *);
+static void str_init(STRING *);
+static void add_more_size(STRING *);
+static int is_max_size(STRING *);
 
 
 /* リソース確保＆初期化（中身を空文字列にする） */
@@ -11,13 +11,13 @@ STRING *str_create(){
     //STRING構造体一つのサイズをmallocで割り当てる。
     STRING *s;
     s = (STRING *)malloc(sizeof(STRING));
-    _str_init(s);
+    str_init(s);
     return s;
 }
 
 /* 文字列をセット */
 void str_set(STRING *s, char * const str){
-    _str_init(s);
+    str_init(s);
     str_add(s, str);
 }
 
@@ -26,8 +26,8 @@ void str_add(STRING *s, char * const str){
     int start = s->count;   //文字列追加開始位置
     for(int i=0; str[i]!='\0'; i++){
         //次の文字を入れるサイズがなければ一定サイズを追加する。
-        if(_is_max_size(s)){
-            _add_more_size(s);
+        if(is_max_size(s)){
+            add_more_size(s);
         }
         s->string[start+i] = str[i];
         s->count++;
@@ -42,8 +42,8 @@ STRING *str_extract(STRING * s1, const int num1, const int num2){
     int i,j;      //i:コピー先文字列用カウンタ, j:コピー元文字列用カウンタ
     for(i=0, j=num1; i<num2; i++,j++){
         //次の文字を入れるサイズがなければ一定サイズを追加する
-        if(_is_max_size(s2)){
-            _add_more_size(s2);
+        if(is_max_size(s2)){
+            add_more_size(s2);
         }
         s2->string[i] = s1->string[j];
         s2->count++;
@@ -68,14 +68,14 @@ void str_destroy(STRING *s){
 }
 
 /*STRING構造体の初期化を行う*/
-static void _str_init(STRING *s){
+static void str_init(STRING *s){
     s->string = (char *)calloc(INITIALIZE_BUFFER_SIZE, sizeof(char));
     s->max_size = INITIALIZE_BUFFER_SIZE;
     s->count = 0;
 }
 
 /*文字列保持用のメモリサイズを追加。追加部分はゼロフィルする。*/
-static void _add_more_size(STRING *s){
+static void add_more_size(STRING *s){
     s->string = (char *)realloc(s->string, s->max_size + ADD_BUFFER_SIZE);
     for(int i=0; i<ADD_BUFFER_SIZE; i++){
         s->string[s->max_size + i] = '\0';
@@ -84,7 +84,7 @@ static void _add_more_size(STRING *s){
 }
 
 /*文字列保持用のメモリサイズが満タンなら1を、そうでなければ0を返す*/
-static int _is_max_size(STRING *s){
+static int is_max_size(STRING *s){
     if(s->max_size == s->count+1){//(null文字用サイズのためにs->count+1となる)
         return 1;
     }else{
