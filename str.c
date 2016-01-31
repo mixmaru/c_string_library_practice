efa0f272b7951bd3e225187d8bdc57d8fd3e1e33
mix <mix.touch777@gmail.com>
2016-01-31 16:27:56 +0900

    文字列に必要な動的メモリ確保を、メモリが足りない場合に、必要量の倍を確保するようにした

--- str.c
+++ str.c
@@ -2,17 +2,18 @@
 #include "str.h"
 
 /*STRING構造体の初期化を行う。最初から10文字(+null文字)のサイズを用意する*/
-static void str_init(STRING *s){
-    s->string = (char *)calloc(1, sizeof(char));
-    s->max_char_num = 1;
-    s->char_num = 0;
+static void str_init(STRING *lib_string){
+    lib_string->string = (char *)malloc(sizeof(char)*11);
+    lib_string->string[0] = '\0';
+    lib_string->max_char_num = 10;
+    lib_string->char_num = 0;
 }
 
-/*STRING構造体の文字列サイズを追加する*/
-static STRING * add_memsize(STRING * s, const int add_size){
-    s->string = (char *)realloc(s->string, s->max_char_num + add_size);
-    s->max_char_num += add_size;
-    return s;
+/*STRING構造体の文字列サイズを追加する。（必要になる文字数の倍の文字数を保持できるようにメモリを確保する）*/
+static STRING * add_memsize(STRING *lib_string, const int new_need_char_num){
+    lib_string->max_char_num = (lib_string->char_num + new_need_char_num) * 2;
+    lib_string->string = (char *)realloc(lib_string->string, lib_string->max_char_num + 1);
+    return lib_string;
 }
 
 /* リソース確保＆初期化（中身を空文字列にする） */
@@ -32,14 +33,18 @@ void str_set(STRING *s, const char * str){
 
 /* 文字列追加 */
 void str_add(STRING *lib_string, const char * str){
-    //必要サイズを追加
-    int add_size = 0;
+    //strの文字数をカウント
+    int str_num = 0;
     char const *tmp_str = str;          //strの走査用ポインタ
     while(*tmp_str != '\0'){
-        add_size++;
+        str_num++;
         tmp_str++;
     }
-    lib_string = add_memsize(lib_string, add_size);
+    //メモリサイズが足りるか判定
+    //足りなければ、新たにメモリサイズを確保する
+    if(lib_string->max_char_num < lib_string->char_num + str_num){
+        lib_string = add_memsize(lib_string, str_num);
+    }
 
     //それぞれの文字列の走査用ポインタを用意
     tmp_str = str;                                                      //strの走査用ポインタ
@@ -59,14 +64,24 @@ void str_add(STRING *lib_string, const char * str){
 /* （第二引数）文字目から（第三引数）文字取り出した新しい文字列を返す */
 STRING *str_extract(STRING * lib_string, const int start, const int chars_num){
     //start, chars_numから文字列コピーの開始位置firstと、コピー文字数copy_numを決定する
-    int first       = (start >= 0)     ? start      : lib_string->char_num + start;
-    int copy_num    = (chars_num >= 0) ? chars_num  : (lib_string->char_num+chars_num)-first+1;
+    int first = (start >= 0)     ? start      : lib_string->char_num + start;
+    int copy_num = 0;
+    if(chars_num >= 0){
+        int can_copy_max_num = lib_string->char_num - first;
+        copy_num = (chars_num >= can_copy_max_num) ? can_copy_max_num : chars_num;
+    }else{
+        copy_num = (lib_string->char_num+chars_num)-first+1;
+    }
 
     //返却用構造体の用意
     STRING *ret_string;
     ret_string = str_create();
-    //必要分のサイズを用意する。
-    ret_string = add_memsize(ret_string, copy_num);
+
+    //メモリサイズが足りるか判定
+    //足りなければ、新たにメモリサイズを確保する
+    if(ret_string->max_char_num < copy_num){
+        ret_string = add_memsize(ret_string, copy_num);
+    }
 
     //それぞれの文字列の走査用ポインタを用意。tmp_lib_stringは開始位置をセット
     char *tmp_ret_string = ret_string->string;
